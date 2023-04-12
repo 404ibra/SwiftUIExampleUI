91493c53cf91a526b9bb249a5fbf55801cfae4e3
404ibra <ibra.aktass@gmail.com>
2023-04-12 20:46:58 +0300

    Initial Commit

---
+++ UIExample/UIExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  UIExampleApp.swift
+//  UIExample
+//
+//  Created by İbrahim Aktaş on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIExampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
